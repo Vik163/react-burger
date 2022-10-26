af6c240b8c4dc678dbaec85464929ab584ae7352
Vik63 <sfoto116@yandex.ru>
2022-10-26 19:41:10 +0400

    создание структуры

---
+++ src/components/App/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+import { data  } from '../../utils/data'
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
